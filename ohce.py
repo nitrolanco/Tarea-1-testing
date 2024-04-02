2ccd9f0172472ea4b027ba24813813926ea702c3
nitrolanco <mamibalss@gmail.com>
2024-04-02 19:11:59 -0300

    Commit 1
    Código refactorizado, test 0 aprobado

--- ohce.py
+++ ohce.py
@@ -1,5 +1,15 @@
-
+import datetime
+def greet(name):
+    current_hour = datetime.datetime.now().hour
+    if 20 <= current_hour < 24 or 0 <= current_hour < 6:
+        print(f'¡Buenas noches {name}!')
+    elif 6 <= current_hour < 12:
+        print(f'¡Buenos días {name}!')
+    elif 12 <= current_hour < 20:
+        print(f'¡Buenas tardes {name}!')
 def main():
+    name = input("¡Hola! ¿Cómo te llamas? ")
+    greet(name)
     return None
 
 if __name__=="__main__":
