3efea2abe0027be2512bf181dcdcb2d4b4b37204
nitrolanco <mamibalss@gmail.com>
2024-04-02 20:47:54 -0300

    Commit 7
    Test 3 pasado
    Código refactorizado

--- ohce.py
+++ ohce.py
@@ -10,6 +10,8 @@ def greet(name):
 
 def reverse_echo(input_str):
     return input_str[::-1]
+def is_palindrome(input_str):
+    return input_str.lower() == input_str.lower()[::-1]
 
 def main():
     name = input("¡Hola! ¿Cómo te llamas? ")
@@ -19,6 +21,9 @@ def main():
         if user_input.lower() == 'stop!':
             print(f'Adiós {name}!')
             break
+        elif is_palindrome(user_input):
+            print(reverse_echo(user_input))
+            print('¡Bonita palabra!')
         else:
             print(reverse_echo(user_input))
 
